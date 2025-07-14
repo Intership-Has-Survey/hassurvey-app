e2e70b77d0727a9f5e0dc6cfe72890a04965e551
Karel <karelriyan@gmail.com>
2025-07-14 15:15:45 +0700

    Fix: Migration Sewa

--- database/migrations/2025_07_07_000005_sewa.php
+++ database/migrations/2025_07_07_000005_sewa.php
@@ -12,7 +12,6 @@ return new class extends Migration {
     {
         Schema::create('sewa', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('jenis_sewa'); 
             $table->date('tgl_mulai');
             $table->date('tgl_selesai');
             $table->string('jenis'); //B2B atau B2C
