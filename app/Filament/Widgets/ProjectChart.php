acb7eb6eb2be91bba8e4a1fc2e86f3f82c0ab52b
SyahrialHipdi <MHsyahrial6@gmail.com>
2025-07-21 13:37:15 +0700

    feat:add user manajemen

--- app/Filament/Widgets/ProjectChart.php
+++ app/Filament/Widgets/ProjectChart.php
@@ -1,56 +1,56 @@
 <?php
 
-namespace App\Filament\Widgets;
+// namespace App\Filament\Widgets;
 
-use Filament\Widgets\ChartWidget;
-use App\Models\Project;
-use App\Models\Kategori;
-use Filament\Forms\Components\DatePicker;
+// use Filament\Widgets\ChartWidget;
+// use App\Models\Project;
+// use App\Models\Kategori;
+// use Filament\Forms\Components\DatePicker;
 
-class ProjectChart extends ChartWidget
-{
-    protected static ?string $heading = 'Project per Kategori';
+// class ProjectChart extends ChartWidget
+// {
+    // protected static ?string $heading = 'Project per Kategori';
 
-    public ?string $filter = 'today';
-    protected function getData(): array
-    {
-        $startDate = $this->filterFormData['start_date'] ?? null;
-        $endDate = $this->filterFormData['end_date'] ?? null;
+    // public ?string $filter = 'today';
+    // protected function getData(): array
+    // {
+    //     $startDate = $this->filterFormData['start_date'] ?? null;
+    //     $endDate = $this->filterFormData['end_date'] ?? null;
 
-        $query = Kategori::query()->withCount([
-            'projects' => function ($query) use ($startDate, $endDate) {
-                if ($startDate && $endDate) {
-                    $query->whereBetween('created_at', [$startDate, $endDate]);
-                }
-            }
-        ]);
+    //     $query = Kategori::query()->withCount([
+    //         'projects' => function ($query) use ($startDate, $endDate) {
+    //             if ($startDate && $endDate) {
+    //                 $query->whereBetween('created_at', [$startDate, $endDate]);
+    //             }
+    //         }
+    //     ]);
 
-        $categories = $query->get();
+    //     $categories = $query->get();
 
-        return [
-            'datasets' => [
-                [
-                    'label' => 'Jumlah Project',
-                    'data' => $categories->pluck('projects_count'),
-                    'backgroundColor' => [
-                        '#FF6384',
-                        '#36A2EB',
-                        '#FFCE56',
-                        '#4BC0C0',
-                        '#9966FF'
-                    ],
-                ],
-            ],
-            'labels' => $categories->pluck('nama_kategori'),
-        ];
-    }
+    //     return [
+    //         'datasets' => [
+    //             [
+    //                 'label' => 'Jumlah Project',
+    //                 'data' => $categories->pluck('projects_count'),
+    //                 'backgroundColor' => [
+    //                     '#FF6384',
+    //                     '#36A2EB',
+    //                     '#FFCE56',
+    //                     '#4BC0C0',
+    //                     '#9966FF'
+    //                 ],
+    //             ],
+    //         ],
+    //         'labels' => $categories->pluck('nama_kategori'),
+    //     ];
+    // }
 
 
 
-    protected function getType(): string
-    {
-        return 'pie'; // atau 'doughnut'
-    }
+    // protected function getType(): string
+    // {
+    //     return 'pie'; 
+    // }
 
     // protected function getFilters(): ?array
     // {
@@ -68,15 +68,15 @@ class ProjectChart extends ChartWidget
     //     ];
     // }
 
-    protected function getFormSchema(): array
-    {
-        return [
-            DatePicker::make('start_date')
-                ->label('Start Date')
-                ->reactive(),
-            DatePicker::make('end_date')
-                ->label('End Date')
-                ->reactive(),
-        ];
-    }
-}
+    // protected function getFormSchema(): array
+    // {
+    //     return [
+    //         DatePicker::make('start_date')
+    //             ->label('Start Date')
+    //             ->reactive(),
+    //         DatePicker::make('end_date')
+    //             ->label('End Date')
+    //             ->reactive(),
+    //     ];
+    // }
+// }
--- app/Filament/Widgets/ProjectTypeOverview.php
+++ app/Filament/Widgets/ProjectTypeOverview.php
@@ -1,19 +1,18 @@
 <?php
 
-namespace App\Filament\Widgets;
+// namespace App\Filament\Widgets;
 
-use Filament\Widgets\StatsOverviewWidget as BaseWidget;
-use Filament\Widgets\StatsOverviewWidget\Stat;
-use App\Models\Project;
+// use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+// use Filament\Widgets\StatsOverviewWidget\Stat;
+// use App\Models\Project;
 
-class ProjectTypeOverview extends BaseWidget
-{
-    protected function getStats(): array
-    {
-        return [
-            Stat::make('Online', Project::query()->where('sumber', 'online')->count()),
-            Stat::make('Offline', Project::query()->where('sumber', 'offline')->count()),
-            // Stat::make('Rabbits', Project::query()->where('kategori', 'rabbit')->count()),
-        ];
-    }
-}
+// class ProjectTypeOverview extends BaseWidget
+// {
+//     protected function getStats(): array
+//     {
+//         return [
+//             Stat::make('Online', Project::query()->where('sumber', 'online')->count()),
+//             Stat::make('Offline', Project::query()->where('sumber', 'offline')->count()),
+//         ];
+//     }
+// }
