0fb84f6d02d27a815ea971c4a8d2224ea764d8eb
Karel <karelriyan@gmail.com>
2025-08-01 09:34:17 +0700

    Feat: kewajiban bayar awal

--- database/migrations/2025_08_01_000009_create_kewajiban_bayar_table.php
+++ database/migrations/2025_08_01_000009_create_kewajiban_bayar_table.php
@@ -12,8 +12,10 @@ return new class extends Migration {
     {
         Schema::create('kewajiban_bayars', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->foreignUuid('operasional_id')->constrained('operasionals')->onDelete('cascade');
+
+            $table->foreignUuid('operasional_id')->nullable()->constrained('operasionals')->onDelete('cascade');
             $table->foreignUuid('penerima_operasional_id')->nullable()->constrained('penerima_operasionals')->onDelete('set null');
+            
             $table->string('nama');
             $table->text('deskripsi')->nullable();
             $table->decimal('nominal', 15, 2);
