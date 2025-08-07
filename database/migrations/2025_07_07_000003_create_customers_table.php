5fe654ceed89d7e0652e5fba9c3b3512b868061d
diaz287 <khafrizadiaz28@gmail.com>
2025-08-07 13:55:06 +0700

    fix column project

--- database/migrations/2025_07_07_000003_create_customers_table.php
+++ database/migrations/2025_07_07_000003_create_customers_table.php
@@ -14,20 +14,13 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        /*Customer dipanggil menggunakan Morph, Customer harus dibuat dari jasa lain
-        tidak bisa dibuat sendiri. Maka dari itu, Customer yang terbagi menjadi 2 jenis
-        yaitu corporate dan perorangan digabung di satu migrasi. Kondisi PIC Perusahaan
-         yang sama dengan Customer maka mengakibatkan relasi many to many antara perusahaan
-         dan customer, pivot antar keduanya juga dibaut di migrasi ini sehingga total ada
-         3 Migrasi*/
-
         //TABEL PERORANGAN
         Schema::create('perorangan', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('nama');
-            $table->string('gender');
-            $table->string('email')->unique();
-            $table->string('telepon')->tel();
+            $table->string('gender')->nullable();
+            $table->string('email')->unique()->nullable();
+            $table->string('telepon')->tel()->nullable();
             // $table->string('alamat');
 
             $table->string('provinsi', 2)->nullable();
@@ -51,15 +44,15 @@ return new class extends Migration {
             $table->uuid('id')->primary();
             $table->string('nama');
             $table->string('nib')->unique()->nullable();
-            $table->string('level');
-            $table->string('email')->unique();
-            $table->string('telepon')->tel();
+            $table->string('level')->nullable();
+            $table->string('email')->unique()->nullable();
+            $table->string('telepon')->tel()->nullable();
 
-            $table->string('provinsi', 2);
-            $table->string('kota', 5);
-            $table->string('kecamatan', 8);
-            $table->string('desa', 13);
-            $table->string('detail_alamat');
+            $table->string('provinsi', 2)->nullable();
+            $table->string('kota', 5)->nullable();
+            $table->string('kecamatan', 8)->nullable();
+            $table->string('desa', 13)->nullable();
+            $table->string('detail_alamat')->nullable();
 
             $table->timestamps();
 
