dd6561685fd86e6e1ef851bf4f7fc829b518d3d3
diaz287 <khafrizadiaz28@gmail.com>
2025-08-11 18:55:26 +0700

    seeder customers company

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -3,13 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    //
-    use HasUuids;
+    use HasFactory, HasUuids;
 
     protected $fillable = ['name'];
 
