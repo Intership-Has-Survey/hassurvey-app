ee76464b3e14e4e8a6b4a70342f0d37cdff5f348
SyahrialHipdi <MHsyahrial6@gmail.com>
2025-08-13 14:36:26 +0700

    fix: bahasa pada activity log

---
+++ lang/vendor/activitylog/id/forms.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'changes' => 'Perubahan',
+    'fields' => [
+        'log_name' => [
+            'label' => 'Jenis',
+        ],
+        'event' => [
+            'label' => 'Peristiwa',
+        ],
+        'subject_type' => [
+            'label' => 'Subjek',
+        ],
+        'causer' => [
+            'label' => 'Pengguna',
+        ],
+        'description' => [
+            'label' => 'Keterangan',
+        ],
+        'properties' => [
+            'label' => 'Properti',
+        ],
+        'created_at' => [
+            'label' => 'Tanggal dibuat',
+        ],
+        'old' => [
+            'label' => 'Tua',
+        ],
+        'attributes' => [
+            'label' => 'Baru',
+        ],
+    ],
+];
